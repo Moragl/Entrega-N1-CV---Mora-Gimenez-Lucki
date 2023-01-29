9acb7948221dd5870509c838437aab0f37476b53
--global <--global>
2023-01-29 11:37:58 -0300

    Proyecto Integrador

---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'grey';
+    document.body.style.color = 'white ';
+    document.querySelector('h1').style.color = 'white';
+})
